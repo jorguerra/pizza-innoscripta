584efad938e1a5226124f1fc47d4ca5abef5eba5
Jorge Guerra <jorguerra@gmail.com>
2019-11-27 23:04:22 -0300

    api_token and admin role added in users

--- app/User.php
+++ app/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password', 'remember_token', 'api_token'
     ];
 
     /**
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function(User $user){
+            $user->api_token = \Str::random(60);
+        });
+    }
 }
