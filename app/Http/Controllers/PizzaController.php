d2290d405b34a07c40071bce753b4305a9586adf
Jorge Guerra <jorguerra@gmail.com>
2019-11-29 19:34:31 -0300

    Pizzas
    Pizzas
    
    Fix in decimal field

---
+++ app/Http/Controllers/PizzaController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Pizza;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PizzaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Pizza::paginate();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        if(!Auth::user()->admin) 
+            return response();
+        $pizza = Pizza::create($request->pizza);
+        return $pizza;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pizza $pizza)
+    {
+        return $pizza;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pizza $pizza)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pizza $pizza)
+    {
+        $pizza->update($request->request->toArray());
+        return $pizza;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Pizza  $pizza
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pizza $pizza)
+    {
+        $pizza->delete();
+        return $pizza;
+    }
+}
---
+++ app/Pizza.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pizza extends Model
+{
+    protected $fillable = ['name', 'photo', 'description', 'price'];
+
+}
