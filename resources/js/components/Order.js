3a9ed077cf5a9341d492cbff53d069da26661504
Jorge Guerra <jorguerra@gmail.com>
2019-11-30 21:10:11 -0300

    Improvement on displaying pizzas in the shopping cart

--- resources/js/components/Order.js
+++ resources/js/components/Order.js
@@ -2,11 +2,56 @@ import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
 axios = require('axios').default;
 
-export default class Cart extends Component{
+class Item extends Component {
+    state = {}
     constructor(props){
         super(props)
-        this.state = {pizzas: []}
+        this.state = {
+            pizza: props.pizza,
+            add: props.add.bind(this, props.pizza.id),
+            remove: props.remove.bind(this, props.pizza.id),
+        }
     }
+    remove(){
+        this.state.remove();
+        let pizza = this.state.pizza;
+        pizza.quantity -= !pizza.quantity ? 0 : 1;
+        this.setState({pizza: pizza})
+    }
+    add(){
+        this.state.add()
+        let pizza = this.state.pizza;
+        pizza.quantity ++;
+        this.setState({pizza: pizza})
+    }
+    
+    render(){
+        const pizza = this.state.pizza;
+        console.log(this.props.pizza, pizza)
+        return (
+            <div className="pricing-entry d-flex ftco-animate fadeInUp ftco-animated">
+                <div className="img" style={pizza.style}></div>
+                <div className="desc pl-3">
+                    <div className="d-flex text align-items-center">
+                        <h3><span>{pizza.quantity} x {pizza.info.name}</span></h3>
+                        <span className="price">${pizza.info.price}</span>
+                    </div>
+                    <div className="d-block order">
+                        Quantity: {pizza.quantity}
+                        <div className="float-right">
+                        <Link to='#' className="text-primary" onClick={this.add.bind(this)}>+</Link>
+                        &nbsp;&nbsp;&nbsp;
+                        <Link to='#' className="text-danger" onClick={this.remove.bind(this)}>-</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default class Cart extends Component{
+    state = {pizzas: [], qty: []}
 
     componentDidMount(){
         this.props.order.map((pizza) => {
@@ -17,13 +62,14 @@ export default class Cart extends Component{
                 this.setState((prevState) => ({pizzas: [pizza, ...prevState.pizzas] }))
             })
         })
+        
     }
 
     render(){
         return (
             <section className="ftco-section">
                 <div className="container">
-                    <div className="row justify-content-center">
+                    <div className="row justify-content-center" style={{marginBottom: '3em'}}>
                         <div className="col-md-7 heading-section text-center ftco-animate fadeInUp ftco-animated"> 
                             <h2 className="mb-4">My Order</h2>
                             <p className="flip">
@@ -34,26 +80,14 @@ export default class Cart extends Component{
                         </div>
                     </div>
                     <div className="row">
-                        <div className="col-md-6">
-                            {this.state.pizzas.map((pizza, index) => 
-                                <div key={pizza.id} className="pricing-entry d-flex ftco-animate fadeInUp ftco-animated">
-                                    <div className="img" style={pizza.style}></div>
-                                    <div className="desc pl-3">
-                                        <div className="d-flex text align-items-center">
-                                            <h3><span>{pizza.quantity} x {pizza.info.name}</span></h3>
-                                            <span className="price">${pizza.info.price}</span>
-                                        </div>
-                                        <div className="d-block order">
-                                            Quantity: {pizza.quantity}
-                                            <div className="float-right">
-                                            <Link to='#' className="text-primary" onClick={this.props.add.bind(this, pizza.id)}>+</Link>
-                                            <Link to='#' className="text-danger" onClick={this.props.remove.bind(this, pizza.id)}>x</Link>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
+                        {this.state.pizzas.map((pizza, index) =>
+                            <div key={index} className="col-md-6">
+                                <Item pizza={pizza}
+                                    remove={this.props.remove.bind(this, pizza.id)}
+                                    add={this.props.add.bind(this, pizza.id)} 
+                                />
+                            </div>
+                        )}
                     </div>
                 </div>
             </section>
