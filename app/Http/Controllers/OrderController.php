6be27e7d4aa38a519d945e50660fe43b34b7a2c7
Jorge Guerra <jguerrag@interior.gob.cl>
2019-12-02 12:58:33 -0300

    session from facades

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Pizza;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Session;
 
 class OrderController extends Controller
 {
